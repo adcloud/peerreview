99c4afa1e95b640df4009744bd11c5f64253be23
Matthias Luebken <matthias.luebken@gmail.com>
2013-03-16 14:17:12 +0100

    new questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ t.members << [m1, m2, m3, m4 ]
 t.save
 
 Question.destroy_all()
-q1 = Question.create({ section: 'Produktivitaet', text: ' trägt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+q1 = Question.create({ section: 'Produktivitaet', text: ' traegt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 q2 = Question.create({ section: 'Produktivitaet', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 q3 = Question.create({ section: 'Verstaendnis', text: '   findet sich schnell zurecht und kann Fragen zuverlaessig beantworten.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 q4 = Question.create({ section: 'Qualitaet', text: ' schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
@@ -35,7 +35,7 @@ Question.create({ section: 'MVP', text: ' beachtet Sprintziele und Umfang der Us
 Question.create({ section: 'Technischen Exzellenz', text: ' erweitert kontinuierlich seine Faehigkeiten/Kenntnisse.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Technischen Exzellenz', text: ' ist der Spezialist in einem oder mehreren Bereichen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Technischen Exzellenz', text: ' ist Ansprechpartner fuer einen oder mehrere Bereichen und viele Kollegen machen davon Gebrauch.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Unternehmerischen Weitsicht', text: ' stellt die Aufgaben in den Kontext von Adcloud und Olis Ideen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: ' stellt die Aufgaben in den Kontext von Adcloud und CTOs Ideen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Unternehmerischen Weitsicht', text: '  versteht die Beduerfnisse der Anwender.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Zusammenarbeit', text: ' bringt gute Stimmung in das Team.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Zusammenarbeit', text: ' ist mir gegenueber zuverlaessig.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
@@ -43,10 +43,85 @@ Question.create({ section: 'Wissensweitergabe', text: ' ist ansprechbar und hilf
 Question.create({ section: 'Wissensweitergabe', text: ' ist ein guter Pair-Partner beim Pair-Programming.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' loest Teamkonflikte.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert die Zusammenarbeit in DEV..', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 
-Answer.destroy_all()
+Question.create({ section: 'PO Allgemein', text: ' s UserStories sind SMART.', forrole_id: role_po.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'PO Allgemein', text: ' s fachliche Vorgaben verändern sich nicht während des Sprints.', forrole_id: role_po.id, fromrole_id: role_dev.id});
+Question.create({ section: 'PO Allgemein', text: ' ist ansprechbar für fachliche Nachfragen während des Sprints.', forrole_id: role_po.id,  fromrole_id: role_dev.id});
+
+Question.create({ section: 'Persönliche Wertschätzung ', text: 'sorgt kontinuierlich dafür, dass sich die Rahmenbedingungen zur Ausübung meiner Aufgaben verbessern. ', forrole_id: role_manager.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Entwicklung', text: 'unterstützt mich in der Erreichung meiner Entwicklungsziele.. ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'Persönliche Wertschätzung ', text: 'nimmt meine Anregungen ernst. ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'Kommunikation ', text: 'ist ansprechbar ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'Kommunikation ', text: ' macht die Dringlichkeit von Themen klar. ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'Kommunikation ', text: ' informiert mich umfassend.', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+Question.create({ section: 'Organisation', text: ' fördert die Selbstorganisation in den Teams und läßt uns als Team  laufen.', forrole_id: role_manager.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Umsetzungsstärke', text: ' setzt meine Vorschläge um', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
+
+Question.create({ section: 'Organisation', text: ' s technische Ideen sind nachvollziehbar.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Persönliche Wertschätzung', text: '  nimmt meine technischen Ideen und Anregungen ernst.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Entwicklung', text: '  lässt mir genügend Freiraum eigene technische Ideen zu entwicklen.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Kommunikation', text: '  macht die Roadmap transparent.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
+
+Question.create({ section: 'Verständis', text: 'findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: 's Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' sucht nach einer möglichst einfachen Lösung.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' holt sich schnell Feedback ein. Z.B. vom PO oder Anwendern mit Mockups oder Prototypen.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' beachtet Sprintziele und Umfang der Userstories.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' erweitert kontinuierlich seine Fähigkeiten/Kenntnisse.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Technischen Exzellenz', text: 'ist der Spezialist in einem oder mehreren Bereichen.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Technischen Exzellenz', text: 'ist Ansprechpartner für einen oder mehrere Bereichen und viele Kollegen machen davon Gebrauch.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Technischen Exzellenz', text: 'findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: 'stellt die Aufgaben in den Kontext von Adcloud und CTOs Ideen.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: 'versteht die Bedürfnisse der Anwender.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Zusammenarbeit', text: ' bringt gute Stimmung in das Team.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Zusammenarbeit', text: ' ist mir gegenüber zuverlässig.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Wissensweitergabe', text: ' ist ansprechbar und hilft kollegial bei Problemen weiter.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Wissensweitergabe', text: 'findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: 'ist ein guter Pair-Partner beim Pair-Programming.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: 'löst Teamkonflikte.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: '  verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
+
+Question.create({ section: 'Produktivität', text: ' trägt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Produktivität', text: ' arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Verständnis', text: '  findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: '  holt sich schnell Feedback ein. Z.B. vom PO oder Anwendern mit Mockups oder Prototypen.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: 's Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' sucht nach einer möglichst einfachen Lösung.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: '  beachtet Sprintziele und Umfang der Userstories.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Unternehmerische Weitsicht', text: ' stellt die Aufgaben in den Kontext von Adcloud und CTOs Ideen.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Unternehmerische Weitsicht', text:  'versteht die Bedürfnisse der Anwender.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Zusammenarbeit', text: ' bringt gute Stimmung in das Team.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Zusammenarbeit', text: ' ist mir gegenüber zuverlässig.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: ' löst Teamkonflikte.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_po.id});
+
+Question.create({ section: 'Produktivität', text: '  trägt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Produktivität', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Verständnis', text: '  findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Qualitaet', text: '  schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Qualitaet', text: ' s Code reviewe ich gerne.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Qualitaet', text: '  ist ein Boyscout! Er hinterlässt Legacy-Code sauberer als vorher?', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Qualiteat', text: '  schreibt automatisierte Tests falls möglich.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: ' sucht nach einer möglichst einfachen Lösung.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: '  holt sich schnell Feedback ein. Z.B. vom PO oder Anwendern mit Mockups oder Prototypen.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Minimal Viable Product / MVP', text: '  beachtet Sprintziele und Umfang der Userstories.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Technischen Exzellenz', text: '  erweitert kontinuierlich seine Fähigkeiten/Kenntnisse.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Technischen Exzellenz', text: '  ist der Spezialist in einem oder mehreren Bereichen.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Technischen Exzellenz', text: '  ist Ansprechpartner für einen oder mehrere Bereichen und viele Kollegen machen davon Gebrauch.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: '" stellt die Aufgaben in den Kontext von Adcloud und Olis Ideen.
+"', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: ' versteht die Bedürfnisse der Anwender.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Zusammenarbeit', text: '  bringt gute Stimmung in das Team.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Zusammenarbeit', text: '  ist mir gegenüber zuverlässig.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Wissensweitergabe', text: '  ist ansprechbar und hilft kollegial bei Problemen weiter.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Wissensweitergabe', text: '  ist ein guter Pair-Partner beim Pair-Programming.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: '  löst Teamkonflikte.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: '  verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
+Question.create({ section: 'Scrum/Dev-Organisation', text: '  verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
 
 
 Review.destroy_all()
-
