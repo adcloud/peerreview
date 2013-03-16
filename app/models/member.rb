5986e178371242ac618b2d0aa1e59bccb644c63f
Matthias Luebken <matthias.luebken@gmail.com>
2013-03-16 18:35:34 +0100

    better name

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,14 @@
 class Member < ActiveRecord::Base
   belongs_to :role
+  belongs_to :team
   #TODO
   def to_s
   	email
   end
+
+  def simple_name
+  	m = email.match /(\w*)/
+  	m.to_s.capitalize
+  end
+
 end
