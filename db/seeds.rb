df36206fdba250d236b4c7c9de377806c82cc53a
Matthias Luebken <matthias.luebken@gmail.com>
2013-03-22 16:15:12 +0100

    new teams data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ role_dev = Role.create({ name: 'Developer'})
 role_po = Role.create({ name: 'Product Owner'})
 role_manager = Role.create({ name: 'Manager'})
 role_arch = Role.create({ name: 'Architect'})
+role_qui = Role.create({ name: 'QuiRe'})
+
 
 Member.destroy_all()
 m1 = Member.create({ email: 'matthias.luebken@adcloud.com', role_id: role_manager.id })
@@ -12,18 +14,47 @@ m3 = Member.create({ email: 'lukas.wehn@adcloud.com', role_id: role_po.id })
 m4 = Member.create({ email: 'tim.schindler@adcloud.com', role_id: role_dev.id })
 m5 = Member.create({ email: 'jan.pieper@adcloud.com', role_id: role_dev.id })
 m6 = Member.create({ email: 'tj.gabbour@adcloud.com', role_id: role_dev.id })
+m7 = Member.create({ email: 'waldemar.schwan@adcloud.com', role_id: role_dev.id })
+m8 = Member.create({ email: 'lars.wolff@adcloud.com', role_id: role_dev.id })
+m9 = Member.create({ email: 'martin.otten@adcloud.com', role_id: role_dev.id })
+m10 = Member.create({ email: 'frank.muenchmeyer@adcloud.com', role_id: role_dev.id })
+m11 = Member.create({ email: 'alfonz.borsos@adcloud.com', role_id: role_dev.id })
+m12 = Member.create({ email: 'tim.buchwaldt@adcloud.com', role_id: role_dev.id })
+m13 = Member.create({ email: 'sven.gebhardt@adcloud.com', role_id: role_dev.id })
+m14 = Member.create({ email: 'otto.poellath@adcloud.com', role_id: role_dev.id })
+m15 = Member.create({ email: 'dirk.breuer@adcloud.com', role_id: role_dev.id })
+m16 = Member.create({ email: 'sebastian.cohnen@adcloud.com', role_id: role_dev.id })
+m17 = Member.create({ email: 'stephan.zeissler@adcloud.com', role_id: role_dev.id })
+m18 = Member.create({ email: 'gabriel.pickard@adcloud.com', role_id: role_dev.id })
+m19 = Member.create({ email: 'ross.crawford@adcloud.net>', role_id: role_dev.id })
+m20 = Member.create({ email: 'sascha.fassmann@adcloud.com', role_id: role_po.id })
+m21 = Member.create({ email: 'verena.wuerbel@adcloud.com', role_id: role_po.id })
+m22 = Member.create({ email: 'sebastian.lautwein@adcloud.com', role_id: role_po.id })
+m23 = Member.create({ email: 'lukas.wehn@adcloud.com', role_id: role_po.id })
+m24 = Member.create({ email: 'alwin.karabiowski@adcloud.com', role_id: role_dev.id })
+m25 = Member.create({ email: 'waldemar.schwan@adcloud.com', role_id: role_dev.id })
+m26 = Member.create({ email: 'frank.muenchmeyer@adcloud.com', role_id: role_po.id })
+m26 = Member.create({ email: 'anna.loew@adcloud.com',  role_id: role_dev.id })
+
 
 Team.destroy_all()
-t = Team.new( { name: 'A-Team'} )
-t.members << [m1, m2, m3, m4 ]
-t.save
+t1 = Team.new( { name: 'Connect'} )
+t1.members << [m1, m2, m24, m4, m14, m15, m5, m3]
+t1.save 
+t2 = Team.new( { name: 'Better Results'} )
+t2.members << [m1, m2, m17, m16, m6, m18, m25, m26, m10  ]
+t2.save
+t3 = Team.new( { name: 'Product Experience'} )
+t3.members << [m1, m2, m11, m8, m21 ]
+t3.save 
+
 
 Question.destroy_all()
-q1 = Question.create({ section: 'Produktivitaet', text: ' traegt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-q2 = Question.create({ section: 'Produktivitaet', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-q3 = Question.create({ section: 'Verstaendnis', text: '   findet sich schnell zurecht und kann Fragen zuverlaessig beantworten.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-q4 = Question.create({ section: 'Qualitaet', text: ' schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-q5 = Question.create({ section: 'Qualitaet', text: 's Code reviewe ich gerne.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Produktivitaet', text: ' traegt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Produktivitaet', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Verstaendnis', text: '   findet sich schnell zurecht und kann Fragen zuverlaessig beantworten.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualitaet', text: ' schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualitaet', text: 's Code reviewe ich gerne.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Qualitaet', text: ' ist ein Boyscout! Er hinterlaesst Legacy-Code sauberer als vorher?', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Qualitaet', text: '  schreibt automatisierte Tests falls moeglich.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
