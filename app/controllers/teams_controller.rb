972ca2cf0bae5da98ab0a446b4579cef17a73b32
Matthias Luebken <matthias.luebken@gmail.com>
2013-03-22 16:14:58 +0100

    removed unnecessary show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,11 +7,6 @@ class TeamsController < ApplicationController
     @teams = Team.all
   end
 
-  # GET /teams/1
-  # GET /teams/1.json
-  def show
-  end
-
   # GET /teams/new
   def new
     @team = Team.new
