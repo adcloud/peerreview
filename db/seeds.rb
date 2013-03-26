bd2579288b86e8a5cc7e362b6a336f8a68ffd24a
Matthias Luebken <matthias.luebken@gmail.com>
2013-03-26 17:41:37 +0100

    cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@ role_qui = Role.create({ name: 'QuiRe'})
 
 
 Member.destroy_all()
-m1 = Member.create({ email: 'matthias.luebken@adcloud.com', role_id: role_manager.id })
+matthias_1 = Member.create({ email: 'matthias.luebken@adcloud.com', role_id: role_manager.id })
+matthias_2 = Member.create({ email: 'matthias.luebken@adcloud.com', role_id: role_manager.id })
+matthias_3 = Member.create({ email: 'matthias.luebken@adcloud.com', role_id: role_manager.id })
 m2 = Member.create({ email: 'timo.derstappen@adcloud.com', role_id: role_arch.id })
 m3 = Member.create({ email: 'lukas.wehn@adcloud.com', role_id: role_po.id })
 m4 = Member.create({ email: 'tim.schindler@adcloud.com', role_id: role_dev.id })
@@ -39,24 +41,25 @@ m26 = Member.create({ email: 'anna.loew@adcloud.com',  role_id: role_dev.id })
 
 Team.destroy_all()
 t1 = Team.new( { name: 'Connect'} )
-t1.members << [m1, m2, m24, m4, m14, m15, m5, m3]
+t1.members << [matthias_1, m2, m24, m4, m14, m15, m5, m3]
 t1.save 
 t2 = Team.new( { name: 'Better Results'} )
-t2.members << [m1, m2, m17, m16, m6, m18, m25, m26, m10  ]
+t2.members << [matthias_2, m2, m17, m16, m6, m18, m25, m26, m10  ]
 t2.save
 t3 = Team.new( { name: 'Product Experience'} )
-t3.members << [m1, m2, m11, m8, m21 ]
+t3.members << [matthias_3, m2, m11, m8, m21 ]
 t3.save 
 
 
 Question.destroy_all()
-Question.create({ section: 'Produktivitaet', text: ' traegt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Produktivitaet', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+# Dev => Dev
+Question.create({ section: 'Produktivität', text: ' traegt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Produktivität', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Verstaendnis', text: '   findet sich schnell zurecht und kann Fragen zuverlaessig beantworten.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Qualitaet', text: ' schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Qualitaet', text: 's Code reviewe ich gerne.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Qualitaet', text: ' ist ein Boyscout! Er hinterlaesst Legacy-Code sauberer als vorher?', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Qualitaet', text: '  schreibt automatisierte Tests falls moeglich.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualität', text: ' schreibt fehlerfreien und nachvollziehbaren Code.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualität', text: 's Code reviewe ich gerne.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualität', text: ' ist ein Boyscout! Er hinterlaesst Legacy-Code sauberer als vorher?', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Qualität', text: '  schreibt automatisierte Tests falls moeglich.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
@@ -76,10 +79,12 @@ Question.create({ section: 'Scrum/Dev-Organisation', text: ' loest Teamkonflikte
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 
+# Dev => PO
 Question.create({ section: 'PO Allgemein', text: ' s UserStories sind SMART.', forrole_id: role_po.id,  fromrole_id: role_dev.id});
 Question.create({ section: 'PO Allgemein', text: ' s fachliche Vorgaben verändern sich nicht während des Sprints.', forrole_id: role_po.id, fromrole_id: role_dev.id});
 Question.create({ section: 'PO Allgemein', text: ' ist ansprechbar für fachliche Nachfragen während des Sprints.', forrole_id: role_po.id,  fromrole_id: role_dev.id});
 
+# Dev => Manager
 Question.create({ section: 'Persönliche Wertschätzung ', text: 'sorgt kontinuierlich dafür, dass sich die Rahmenbedingungen zur Ausübung meiner Aufgaben verbessern. ', forrole_id: role_manager.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Entwicklung', text: 'unterstützt mich in der Erreichung meiner Entwicklungsziele.. ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
 Question.create({ section: 'Persönliche Wertschätzung ', text: 'nimmt meine Anregungen ernst. ', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
@@ -89,11 +94,13 @@ Question.create({ section: 'Kommunikation ', text: ' informiert mich umfassend.'
 Question.create({ section: 'Organisation', text: ' fördert die Selbstorganisation in den Teams und läßt uns als Team  laufen.', forrole_id: role_manager.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Umsetzungsstärke', text: ' setzt meine Vorschläge um', forrole_id: role_manager.id,  fromrole_id: role_dev.id});
 
+# Dev => Architect
 Question.create({ section: 'Organisation', text: ' s technische Ideen sind nachvollziehbar.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Persönliche Wertschätzung', text: '  nimmt meine technischen Ideen und Anregungen ernst.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Entwicklung', text: '  lässt mir genügend Freiraum eigene technische Ideen zu entwicklen.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Kommunikation', text: '  macht die Roadmap transparent.', forrole_id: role_arch.id, fromrole_id: role_dev.id});
 
+# Manager => Dev
 Question.create({ section: 'Verständis', text: 'findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
 Question.create({ section: 'Minimal Viable Product / MVP', text: 's Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
 Question.create({ section: 'Minimal Viable Product / MVP', text: ' sucht nach einer möglichst einfachen Lösung.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
@@ -114,6 +121,7 @@ Question.create({ section: 'Scrum/Dev-Organisation', text: 'löst Teamkonflikte.
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
 Question.create({ section: 'Scrum/Dev-Organisation', text: '  verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_manager.id});
 
+# PO => Dev
 Question.create({ section: 'Produktivität', text: ' trägt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_po.id});
 Question.create({ section: 'Produktivität', text: ' arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_po.id});
 Question.create({ section: 'Verständnis', text: '  findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_po.id});
@@ -129,6 +137,7 @@ Question.create({ section: 'Scrum/Dev-Organisation', text: ' löst Teamkonflikte
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert den Scrum Prozess.', forrole_id: role_dev.id, fromrole_id: role_po.id});
 Question.create({ section: 'Scrum/Dev-Organisation', text: ' verbessert die Zusammenarbeit in DEV.', forrole_id: role_dev.id, fromrole_id: role_po.id});
 
+# Arch => Dev
 Question.create({ section: 'Produktivität', text: '  trägt zum Team-Erfolg bei.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
 Question.create({ section: 'Produktivität', text: '  arbeitet effizient.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
 Question.create({ section: 'Verständnis', text: '  findet sich schnell zurecht und kann Fragen zuverlässig beantworten.', forrole_id: role_dev.id, fromrole_id: role_arch.id});
