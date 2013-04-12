d0593b08176b137256001a166a8e71f8ee730b93
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-12 23:22:20 +0200

    TODO für question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+	#TODO should be immutable once their is a answer to this question
   belongs_to :fromrole, class_name: Role
   belongs_to :forrole, class_name: Role
   #TODO
