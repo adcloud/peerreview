9575a58795058f2b3ebe1e1d76009fdb9662ffdb
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-12 23:22:47 +0200

    reworked questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,15 +63,13 @@ Question.create({ section: 'Qualität', text: 's Code reviewe ich gerne.', forro
 Question.create({ section: 'Qualität', text: ' ist ein Boyscout! Er hinterlaesst Legacy-Code sauberer als vorher?', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Qualität', text: '  schreibt automatisierte Tests falls moeglich.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'MVP', text: ' s Umsetzung passt zu den fachlichen Anforderungen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: '  sucht nach einer moeglichst einfachen Loesung.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: '  holt sich schnell Feedback ein. Z.B. vom PO oder Anwendern mit Mockups oder Prototypen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'MVP', text: ' beachtet Sprintziele und Umfang der Userstories.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Technischen Exzellenz', text: ' erweitert kontinuierlich seine Faehigkeiten/Kenntnisse.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Technischen Exzellenz', text: ' ist der Spezialist in einem oder mehreren Bereichen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Technischen Exzellenz', text: ' ist Ansprechpartner fuer einen oder mehrere Bereichen und viele Kollegen machen davon Gebrauch.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
-Question.create({ section: 'Unternehmerischen Weitsicht', text: ' stellt die Aufgaben in den Kontext von Adcloud und CTOs Ideen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
+Question.create({ section: 'Unternehmerischen Weitsicht', text: ' stellt die Aufgaben in den Kontext von Adcloud und den Roadmap Ideen.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Unternehmerischen Weitsicht', text: '  versteht die Beduerfnisse der Anwender.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Zusammenarbeit', text: ' bringt gute Stimmung in das Team.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
 Question.create({ section: 'Zusammenarbeit', text: ' ist mir gegenueber zuverlaessig.', forrole_id: role_dev.id, fromrole_id: role_dev.id});
