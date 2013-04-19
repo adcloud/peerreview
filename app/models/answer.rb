10b02e29de2aea976400719128f0e5f63c939979
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-19 12:27:50 +0200

    forcing a default order as a bugfix for a postgres ordering problem

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :for_member, class_name: Member
   belongs_to :question
   belongs_to :review
+
+  # Force a default order
+  default_scope order("created_at ASC")
 end
