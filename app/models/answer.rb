bc45a898fefcbc8868fa310c83ba83b0daefafe7
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-23 09:45:32 +0200

    validation of text length

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,10 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :review
 
+  #TODO if validation fails in review show error message / notification
+  validates_length_of :text, :maximum => 255
+
+
   # Force a default order
   default_scope order("created_at ASC")
 end
