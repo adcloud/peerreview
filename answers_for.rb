86b196e76fad81c71648514c231cd27d690ada01
Matthias Luebken <matthias.luebken@gmail.com>
2013-06-12 16:20:57 +0200

    bug fix and csv export

--- answers_for.rb
+++ answers_for.rb
@@ -1,3 +1,8 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+# a simple script to get all answers for a member
+
 name = "Alfonz"
 member_id = Member.where(email: 'alfonz.borsos@adcloud.com').first!.id
 sql = 'SELECT questions.section, questions.text as question, answers.text as answer FROM "answers" INNER JOIN "questions" ON "questions"."id" = "answers"."question_id" WHERE "answers"."for_member_id" = ' + member_id.to_s + ' ORDER BY questions.created_at DESC'
@@ -10,32 +15,36 @@ answers.each do |a|
   if results[a["section"]] == nil 
     results[a["section"]] = Hash.new 
   end 
-  #create questions
+  #create question and answer choices
   if results[a["section"]][a["question"]] == nil 
-    results[a["section"]][a["question"]] = Hash.new 
-  end 
-  #create answer choices
-  if results[a["section"]][a["question"]][a["answer"]] == nil 
     results[a["section"]][a["question"]] = {"Sehr zutreffend" => 0, "Eher zutreffend" => 0, "Ein wenig zutreffend" => 0, "Eher nicht zutreffend" => 0, "Nicht beantwortet" => 0}
   end 
   if results[a["section"]][a["question"]][a["answer"]] != nil 
-    results[a["section"]][a["question"]][a["answer"]] = results[a["section"]][a["question"]][a["answer"]] +1 
+    results[a["section"]][a["question"]][a["answer"]] += 1
   else 
-    results[a["section"]][a["question"]]["Nicht beantwortet"] = results[a["section"]][a["question"]]["Nicht beantwortet"] + 1
+    results[a["section"]][a["question"]]["Nicht beantwortet"] += 1
   end
 end
 
 
-puts 'Results for ' + name + ';;;;'
+puts results
+
+#puts 'Results for ' + name + ';;;;'
 #puts 'SECTION; NAME; QUESTION; Sehr zutreffend; Eher zutreffend; Ein wenig zutreffend; Eher nicht zutreffend; Nicht beantwortet'
 
-results.keys.sort.each do |section|
-  results[section].keys.sort.each do |question|
-    print section + "; " + name + "; "+ question + "; "
-    results[section][question].keys.each do |answer|
+
+results.keys.each do |section|
+  results[section].keys.each do |question|
+    if section != nil && name != nil && question != nil
+      print section + "; " + name + "; "+ question + "; "
+      results[section][question].keys.each do |answer|
       #print answer.to_s + ":" + results[section][question][answer].to_s + " ; "
-      print answer.to_s + ":" + results[section][question][answer].to_s + " ; "
+        if results[section][question][answer] != nil && answer != nil
+          #print answer
+          print answer.to_s + ":" + results[section][question][answer].to_s + " ; "
+        end
+      end
+      puts ""
     end
-    puts ""
   end
 end
